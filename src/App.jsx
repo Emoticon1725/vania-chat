c3fb26aa9b3d55cb2439b331e990488fd3a3585a
Anthony Briggs <122236522+Emoticon1725@users.noreply.github.com>
2024-09-15 17:16:39 -0400

    Update App.jsx
    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Login from "./pages/Login";
 import ChatRoom from "./pages/ChatRoom";
 import { PrivateRoute } from "./routes/PrivateRoute";
 import { AuthProvider } from "./context/AuthContext";
-import NotFound from "./pages/NotFound"; // Add this import
 
 function App() {
 
