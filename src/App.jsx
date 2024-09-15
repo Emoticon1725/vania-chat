d9f7b74bbf05157e7a1f5d34cb2b70dc1d60602e
Anthony Briggs <122236522+Emoticon1725@users.noreply.github.com>
2024-09-15 17:39:01 -0400

    Update App.jsx
    next update

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,26 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import Login from "./pages/Login";
 import ChatRoom from "./pages/ChatRoom";
-import { PrivateRoute } from "./routes/PrivateRoute";
-import { AuthProvider } from "./context/AuthContext";
+import Login from "./pages/Login";
+import { PrivateRoute } from "./components/routes/PrivateRoute";
 
 function App() {
-
   return (
-    <Router>
-    <AuthProvider>
-    <Navbar />
-    <Routes>
-      <Route path="/" element={<Login />} />
-      <Route
-        path="/chat"
-        element={
+      <AuthProvider>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/chat"
+          element={
           <PrivateRoute>
             <ChatRoom />
           </PrivateRoute>
-        }
+          }
         />
-       </Routes>
-    </AuthProvider>
-    </Router>
-    
-    
-    
-    
-  )
+      </Routes>
+   </AuthProvider> 
+  );
 }
 
-export default App
+export default App;
